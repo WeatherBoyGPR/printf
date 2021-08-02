2322278185f3537732c8cf775cd119da66a98eeb
Gino Rossi <gp.rossijr@gmail.com>
2021-08-02 21:42:46 +0000

    updated specfunc1_%scrR_.c

--- specfunc1_%scrR_.c
+++ specfunc1_%scrR_.c
@@ -1,5 +1,22 @@
 #include "holberton.h"
+/*
+int functtest(int n, ...)
+{
+	va_list args;
+	va_start(args, n);
 
+	n = mod_specprint(args, -1, -1, -1, 0);
+	n = s_specprint(args, -1, -1, -1, n);
+	va_end(args);
+	return (n);
+}
+
+int main(void)
+{
+	functtest(1);
+	return (0);
+}
+*/
 
 /*DOCUMENTATION TEMPLATE, REPLACE # WITH SPEC CHARACTER*/
 /**
@@ -14,7 +31,7 @@
 int #_specprint(va_list args, int wid, int pri, int len, unsigned int con);
 {
 
-        if (wid => 0 || pri => 0 || len => 0 || args == NULL)
+        if (!(wid == -1 && pri == -1 && len == -1 && args != NULL))
                 return(con);
 
 	[code here]
@@ -24,20 +41,20 @@ int #_specprint(va_list args, int wid, int pri, int len, unsigned int con);
 */
 
 /**
- * %_specprint - will print a percentage sign
+ * mod_specprint - will print a percentage sign
  * @args: va_list to obtain variable from
  * @wid: width modifier to print variable with, -1 if unused
  * @pri: precision modifier to print variable with, -1 if unused
  * @len: length modifier to print variable with, -1 if unused
  * @con: variable to keep track of number of specifier functions
  */
-int %_specprint(va_list args, int wid, int pri, int len, unsigned int con);
+int mod_specprint(va_list args, int wid, int pri, int len, unsigned int con)
 {
 
-	if (wid => 0 || pri => 0 || len => 0 || args == NULL)
+	if (!(wid == -1 && pri == -1 && len == -1 && args != NULL))
 		return(con);
 
-	_putchar(%);
+	_putchar('%');
 
 	return (con + 1);
 }
@@ -50,18 +67,23 @@ int %_specprint(va_list args, int wid, int pri, int len, unsigned int con);
  * @len: length modifier to print variable with, -1 if unused
  * @con: variable to keep track of number of specifier functions
  */
-int s_specprint(va_list args, int wid, int pri, int len, unsigned int con);
+int s_specprint(va_list args, int wid, int pri, int len, unsigned int con)
 {
-	char *s;
+	char *s, *err = "(null)";
 
-	if (wid => 0 || pri => 0 || len => 0 || args == NULL)
+	if (!(wid == -1 && pri == -1 && len == -1 && args != NULL))
 		return (con);
 
-	s = va_arg(args);
+	s = va_arg(args, char *);
 	if (s == NULL)
+	{
+		write(1, err, _strlen(err));
+		printf("%sWCWCWCWC", err);
 		return (con);
+	}
 
-	write(1, s, _strlen(s));
+	printf("TEST SPEC PRINT S");
+	/*write(1, s, _strlen(s));*/
 
 	return (con + 1);
 }
@@ -74,16 +96,17 @@ int s_specprint(va_list args, int wid, int pri, int len, unsigned int con);
  * @len: length modifier to print variable with, -1 if unused
  * @con: variable to keep track of number of specifier functions
  */
-int c_specprint(va_list args, int wid, int pri, int len, unsigned int con);
+int c_specprint(va_list args, int wid, int pri, int len, unsigned int con)
 {
 	char c;
 
-	if (wid => 0 || pri => 0 || len => 0)
+	if (!(wid == -1 && pri == -1 && len == -1 && args != NULL))
 		return(con);
 
-	c = va_arg(args);
+	c = va_arg(args, int);
 
 	_putchar(c);
+
 
 	return (con + 1);
 }
@@ -96,14 +119,14 @@ int c_specprint(va_list args, int wid, int pri, int len, unsigned int con);
  * @len: length modifier to print variable with, -1 if unused
  * @con: variable to keep track of number of specifier functions
  */
-int r_specprint(va_list args, int wid, int pri, int len, unsigned int con);
+int r_specprint(va_list args, int wid, int pri, int len, unsigned int con)
 {
 	char *s;
 
-	if (wid => 0 || pri => 0 || len => 0)
+	if (!(wid == -1 && pri == -1 && len == -1 && args != NULL))
 		return(con);
 
-	s = va_arg(args);
+	s = va_arg(args, char *);
 	if (s == NULL)
 		return (con);
 
@@ -113,15 +136,26 @@ int r_specprint(va_list args, int wid, int pri, int len, unsigned int con);
 	return (con + 1);
 }
 
-/*
- *int R_specprint(va_list args, int wid, int pri, int len, unsigned int con);
- *{
- *	if (wid => 0 || pri => 0 || len => 0)
- *		return(con);
- *
- *
- *
- *
- *	return (con + 1);
- *}
-*/
+/**
+ * #_specprint - will print a
+ * @args: va_list to obtain variable from
+ * @wid: width modifier to print variable with, -1 if unused
+ * @pri: precision modifier to print variable with, -1 if unused
+ * @len: length modifier to print variable with, -1 if unused
+ * @con: variable to keep track of number of specifier functions
+ */
+int R_specprint(va_list args, int wid, int pri, int len, unsigned int con)
+{
+	char *s;
+
+	if (!(wid == -1 && pri == -1 && len == -1 && args != NULL))
+		return (con);
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+		return (con);
+
+	s = rot13(s);
+	write(1, s, _strlen(s));
+	return (con + 1);
+}
