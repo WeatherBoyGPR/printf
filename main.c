6c4b34d0b68a1cf6de7659be1a9bdd9f8261354e
johanne101 <jlopez0493@gmail.com>
2021-08-04 19:06:48 +0000

    [ADD] main file seperate

---
+++ main.c
@@ -0,0 +1,12 @@
+
+int main(void)
+{
+	/*printf("STARTMAIN");
+	printf("Character:[%c]\n", 'G');
+	printf("START_PRINT");*/
+	_printf("Character:[%s]\n", "APPLESAPPLESAPPLES\n");
+
+	/*printf("END_PRINT");
+	printf("ENDMAIN");*/
+	return (0);
+}
