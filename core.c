03484d7464994640b11f16656f2803b454236073
Gino Rossi <gp.rossijr@gmail.com>
2021-08-06 13:22:06 +0000

    updated core.c

--- core.c
+++ core.c
@@ -7,12 +7,12 @@ int core_logic(const char *format, printf_t *ref, talley_t **tal, va_list box)
 {
 	int index = 0, y = 1;/*, tem = 0;*/
 	int buf_w = 0, buf_l = 0, buf_pri = 0;
-	int cnt = 0;
+	int cnt = 0, tot = tal[0]->pri;
 
 	while (format[index]!= '\0')
 	{
 		/*printf("TEST1");*/
-		if (tal[0]->pri && tal[y]->idx == index && ref != NULL && box != NULL)
+		if (tot && tal[y]->idx == index && ref != NULL && box != NULL)
 		{
 			buf_w = tal[y]->wid;
 			buf_l = tal[y]->lensym;
@@ -21,7 +21,7 @@ int core_logic(const char *format, printf_t *ref, talley_t **tal, va_list box)
 			/*printf("TEST2");*/
 			cnt = (ref[tal[y]->spec].print)(box, buf_w, buf_pri, buf_l, cnt);
 			index += tal[y]->len;
-			tal[0]->pri -= 1;
+			tot -= 1;
 		}
 		else if (format[index] != '\0')
 		{
